75c649c9597022b9cc3e8d65456fca10878d00a0
RehanAli <rali0010@student.monash.edu>
2024-01-21 15:31:46 +1100

    ui changes

--- components/Product.tsx
+++ components/Product.tsx
@@ -15,14 +15,14 @@ interface Props {
 function Product({ product }: Props) {
 
     return (
-        <div className='flex h-[400px] w-[320px] select-none flex-col rounded-xl sm:h-[300px]   md:h-[500px] md:w-[400px] cursor-pointer hover:scale-105 transition-all duration-500'>
+        <div className='flex h-[300px] w-[100%] lg:w-[320px] select-none flex-col rounded-xl sm:h-[300px] lg:h-[400px]   md:h-[500px] md:w-[400px] cursor-pointer hover:scale-105 transition-all duration-500'>
             <div className='relative h-[100%] top-9 w-full '>
                 <Link href={product.url} target='_blank'>
                 <Image alt='' src={urlFor(product.image[0]).url()} layout='fill' objectFit='cover' className='rounded-xl ' />
                 </Link>
             </div>
 
-            <div className='space-y-2 text-2xl text-white hover:text-red-700 md:text-2xl lg:text-3xl font-bold relative left-6 bottom-8 z-40'>
+            <div className='space-y-2 text-xl text-white hover:text-red-700 md:text-2xl lg:text-3xl font-bold relative left-6 bottom-8 z-40'>
                 <p>{product.title}</p>
             </div>
 
